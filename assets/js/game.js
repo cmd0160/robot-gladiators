854d23954312b873f3988b7a61faf869bb5aa40e
cdtest <corydavis1989@gmail.com>
2021-11-16 16:24:50 -0600

    console-log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! Javascript is running!")
+var playerName = window.prompt("What is your robot's name?");
+console.log("this logs a String, good for leaving yourself a Message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+console.log("Tony the Robot" + " is ready for battle!");
+console.log(playerName + " is ready for battle!");
+console.log("Your robot, " + playerName + ", has won!");
+
+function fight() {
+    window.alert("The fight has begun");
+}
+// fight();
