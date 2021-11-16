4a6942a23adbf2b1a27bdbf181e1e56e81efd515
cdtest <corydavis1989@gmail.com>
2021-11-16 17:33:42 -0600

    Player and Enemy Health addition

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,41 @@
-var playerName = window.prompt("What is your robot's name?");
-console.log("this logs a String, good for leaving yourself a Message");
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
-console.log("Tony the Robot" + " is ready for battle!");
-console.log(playerName + " is ready for battle!");
-console.log("Your robot, " + playerName + ", has won!");
 
-function fight() {
-    window.alert("The fight has begun");
-}
-// fight();
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+  playerHealth = playerHealth - enemyAttack;
+  console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
+
+// check player's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+    // check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+};
+
+fight();
